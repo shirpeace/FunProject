b46d22b34e5cac86c3cc6ebd76170ad9a2bccc8a
Matan <Matan@Matan-HP>
2016-04-27 10:50:24 +0300

    its me

--- FunProject/src/MainClass.java
+++ FunProject/src/MainClass.java
@@ -21,6 +21,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
 		System.out.println("hello");
+		System.out.println("Its me...");
 		
 		/*try {
 			WordMatrix queries = getWordMatrixFromFile(docFileName);
